8b62a675a7714c0074ed6c97d46ff408724884a9
pekeleku <alumanwalter@gmail.com>
2023-04-23 21:27:48 +0800

    refactor edit profile member => pages

--- components/organisms/SideBar/index.tsx
+++ components/organisms/SideBar/index.tsx
@@ -2,19 +2,39 @@ import Footer from "./Footer";
 import MenuItem from "./MenuItem";
 import Profile from "./Profile";
 
-export default function SideBar() {
+interface SideBarProps {
+  activeMenu: "overview" | "transactions" | "settings";
+}
+
+export default function SideBar(props: SideBarProps) {
+  const { activeMenu } = props;
   return (
     <section className="sidebar">
       <div className="content pt-50 pb-30 ps-30">
         <Profile />
         <div className="menus">
-          <MenuItem title="OverView" icon="ic-menu-overview" active />
-          <MenuItem title="Transactions" icon="ic-menu-transaction" />
-          <MenuItem title="Messages" icon="ic-menu-message" />
-          <MenuItem title="Card" icon="ic-menu-card" />
-          <MenuItem title="Rewards" icon="ic-menu-rewards" />
-          <MenuItem title="Settings" icon="ic-menu-settings" />
-          <MenuItem title="Log Out" icon="ic-menu-logout" />
+          <MenuItem
+            title="OverView"
+            icon="ic-menu-overview"
+            active={activeMenu === "overview"}
+            href="/member"
+          />
+          <MenuItem
+            title="Transactions"
+            icon="ic-menu-transaction"
+            href="/member/transactions"
+            active={activeMenu === "transactions"}
+          />
+          <MenuItem title="Messages" icon="ic-menu-message" href="/member" />
+          <MenuItem title="Card" icon="ic-menu-card" href="/member" />
+          <MenuItem title="Rewards" icon="ic-menu-rewards" href="/member" />
+          <MenuItem
+            title="Settings"
+            icon="ic-menu-settings"
+            href="/member/edit-profile"
+            active={activeMenu === "settings"}
+          />
+          <MenuItem title="Log Out" icon="ic-menu-logout" href="/sign-in" />
         </div>
         <Footer />
       </div>
